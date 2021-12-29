4f22d26051bcd9d4e8d0e567e5e106e554800c7c
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2021-12-29 13:38:53 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (84.95%) - LeetHub

--- perfect-squares/perfect-squares.cpp
+++ perfect-squares/perfect-squares.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int numSquares(int n) {
         if(n<=0) return n;
-        
-        vector<int>dp(n+1, INT_MAX);
+         static vector<int> dp({0});
+        // vector<int>dp(n+1, INT_MAX);
         dp[0]=0;
-        for(int i=1;i<=n;i++){
+        while(dp.size()<=n){
+            int i = dp.size();
+            int ans = INT_MAX;
             for(int j=1;j*j<=i;j++){
-                dp[i]= min(dp[i], dp[i-j*j]+1 );
+                ans= min(ans, dp[i-j*j]+1 );
             }
+            dp.push_back(ans);
         }
         return dp[n];
     }
