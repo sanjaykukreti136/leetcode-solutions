477ef08b105d9bc2f7648df47a62eec2762d900f
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-01-04 09:52:11 +0530

    Time: 14 ms (10.21%), Space: 15 MB (25.99%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    void check(int n , int m,int i, int j, string s, vector<string>&ans){
+        if( i==n && j==m){
+            ans.push_back(s);
+            
+            cout<<s<<"\n";
+            return ;
+        }else if(i>n || j>m || j>i) return ;
+        
+        // if( i>=j ){
+            check(n , m ,i+1 , j , s+'(' , ans);
+            // check(n , m , i , j+1 , s+')' , ans);
+        // }else{
+            check(n , m , i , j+1 , s+')' , ans);
+            // check(n , m , i+1 , j , s+'(' , ans);
+      // /  }
+        
+        
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        string s = "";
+        vector<string>v;
+        check(n , n,0,0 , s , v);
+        return v;
+    }
+};
