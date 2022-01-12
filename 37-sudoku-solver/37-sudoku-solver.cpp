3904385fb8f6f994356c97dec2dbbeee22fc0fc2
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-01-12 06:19:07 +0530

    Time: 47 ms (26.64%), Space: 6.5 MB (27.11%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-  bool issafe(vector<vector<char>>&board, int x, int y,int val){
+  bool issafe(vector<vector<char>>&board, int val, int x, int y){
         for(int i=0;i<9;i++){
             if(board[x][i]==val+'0'){
                 return false;
@@ -23,43 +23,37 @@ public:
         
     }
  bool check(vector<vector<char>>&v, int i, int j){
-    // if(i==9 && j==0) return true;
-    if(j==9 && i==8){
-            return true;
-        }
-        if(j==9){
-            j=0;
-            i++;
-        }
+    if(i==9 && j==0) return true;
+   
     if(v[i][j]=='.'){
     for(int k=1;k<=9;k++){
-        if(issafe(v, i ,j , k  )){
-            // int nrow, ncol;
-            // if(j>=8){
-            //     nrow = i+1;
-            //     ncol =0;
-            // }else{
-            //     nrow = i;
-            //     ncol = j+1;
-            // }
+        if(issafe(v, k,i ,j   )){
+            int nrow, ncol;
+            if(j>=8){
+                nrow = i+1;
+                ncol =0;
+            }else{
+                nrow = i;
+                ncol = j+1;
+            }
             v[i][j]= k+'0';
             // check
             if(i==8 && j==8) return true;
-            if(check(v, i, j+1)) return true;
+            if(check(v, nrow, ncol)) return true;
             v[i][j] = '.';
         }
     }
     }
      else{
-         // int nrow, ncol;
-         //    if(j>=8){
-         //        nrow = i+1;
-         //        ncol =0;
-         //    }else{
-         //        nrow = i;
-         //        ncol = j+1;
-         //    }
-        if(check(v, i,j+1)) return true;
+         int nrow, ncol;
+            if(j>=8){
+                nrow = i+1;
+                ncol =0;
+            }else{
+                nrow = i;
+                ncol = j+1;
+            }
+        if(check(v, nrow, ncol)) return true;
          
      }
     return false;
