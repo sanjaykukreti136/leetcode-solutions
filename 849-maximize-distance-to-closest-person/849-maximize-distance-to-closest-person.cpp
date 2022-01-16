1761d36895db3f9e7b62e0fd91bb7261b8d37cad
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-01-16 21:31:27 +0530

    Time: 11 ms (81.05%), Space: 17 MB (93.10%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -1,45 +1,26 @@
 class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
-        vector<int>left;
-        vector<int>right;
-        
-        int p = INT_MAX;
+        int left = -1; 
+        int ans= 0;
+        int n= seats.size();
         for(int i=0;i<seats.size();i++){
             if(seats[i]==1){
-                left.push_back(0);
-                p = i;
-            }else{
-                // left.p
-                if(p==INT_MAX) left.push_back(p);
-                else left.push_back(i-p);
+                
+                if(left == -1){
+                    ans = max(ans , i);
+                }else{
+                    ans = max(ans, (i-left)/2);
+                }
+                left = i;
+                
             }
         }
         
-        // 1 2 3 1 
-        // 3  2  1  1
-        
-        p = INT_MAX;
-        for(int i=seats.size()-1;i>=0;i--){
-            if(seats[i]==1){
-                right.push_back(0);
-                p = i;
-            }else{
-                // left.p
-                if(p==INT_MAX) right.push_back(p);
-                else right.push_back(p-i);
-            }
-        }
-        reverse(right.begin(),right.end());
-        int ans = 0;
-        for(int i=0;i<seats.size();i++){
-        if(seats[i]==0){
-            cout<<left[i]<<" - "<<right[i]<<"\n";
-            ans = max(ans , min(left[i], right[i]) );
-        }
+        if(seats[n-1]==0){
+            ans = max(ans , n - 1 -  left);
         }
         return ans;
-        
         
     }
 };
