20b9ca7775555c3479bff8fb36732e37f2d6efbc
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-01-21 23:12:26 +0530

    Time: 12 ms (86.50%), Space: 8.6 MB (75.58%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-    
-    bool isEqual(map<char,int>m1, map<char,int>m2){
-        for(auto i : m1){
-            if(m2.find(i.first)==m2.end() || m2[i.first]!=i.second) return false;
-            m2.erase(i.first);
-        }
-        return m2.size()==0;
-    }
-    
     vector<int> findAnagrams(string s, string p) {
-        map<char,int>m1;
-        for(auto i : p) m1[i]++;
-        int i=0, j=0;
-        map<char,int>m2;
-        for(i=0;i<p.length();i++){
-            m2[s[i]]++;
-        }
         vector<int>ans;
-        while(i < s.length()){
-            if(isEqual(m1 , m2)==true){
-                ans.push_back(j);
+        if(s.length() < p.length()) return ans;
+        vector<int>st(26 , 0);
+        vector<int>pt(26,0);
+        for(auto i : p)  pt[i-'a']++;
+        int i=0, j=0;
+        while(j<s.length()){
+            char ch = s[j];
+            st[ch-'a']++;
+            while(st[ch-'a'] > pt[ch-'a']){
+                st[s[i]-'a']--;
+                i++;
             }
-            m2[s[i]]++;
-            
-            m2[s[j]]--;
-            if(m2[s[j]]==0) m2.erase(s[j]);
-            i++;
+            if(j-i+1 == p.length()){
+                ans.push_back(i);
+            }
             j++;
         }
-        if(isEqual(m1 , m2)==true){
-                ans.push_back(j);
-        }
-        
-        
         return ans;
     }
 };
