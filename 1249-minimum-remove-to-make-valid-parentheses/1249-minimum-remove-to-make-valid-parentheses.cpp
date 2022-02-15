796956741d2e0bfbaf51c9dd0a2d109af536ed72
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-02-15 10:39:22 +0530

    Time: 49 ms (20.51%), Space: 12.4 MB (17.40%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        // vector<int>idx;
+        map<int, int>m;
+        stack<int>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]==')'){
+                
+                if(st.empty() || s[i]=='('){
+                    st.push(i);
+                }else if(s[i]==')' && s[st.top()]=='('){
+                    st.pop();
+                }else{
+                    st.push(i);
+                }
+                
+            }
+        }
+        
+        while(!st.empty()){
+            m[st.top()]= 1;
+            st.pop();
+        }
+        string ans ="";
+        for(int i=0;i<s.size();i++){
+            if(m.find(i)==m.end()) ans.push_back(s[i]);
+        }
+        return ans;
+        
+    }
+};
