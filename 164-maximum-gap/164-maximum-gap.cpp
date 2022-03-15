0c304fbd8aa9ed35e64139c99e65d01976b32830
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-03-15 11:27:21 +0530

    Time: 219 ms (73.50%), Space: 78.3 MB (43.58%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.cpp
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        
-        priority_queue<int,vector<int>>pq;
+        int minv = INT_MAX;
+        int maxv = INT_MIN;
         for(auto i : nums){
-            pq.push(i);
+            minv = min(i, minv);
+            maxv = max(i, maxv);
+        }
+        int n = nums.size();
+        if(n<2) return 0;
+    
+        int interval = ceil((maxv-minv+0.0)/(n-1));
+        int buk = n-1;
+        vector<int>maxi(buk, -1);
+        vector<int>mini(buk , INT_MAX);
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]== minv || nums[i]==maxv) continue;
+            int idx = ((nums[i]-minv)/(interval));
+            
+            maxi[idx] = max(maxi[idx], nums[i]);
+            mini[idx]  = min(mini[idx], nums[i]);
+            
         }
         
-        int prev = pq.top() ;
-        pq.pop();
-        int ans= 0;
-        while(!pq.empty()){
-            int a = pq.top();
-            pq.pop();
-            ans = max(ans, abs(a-prev));
-            prev  = a;
+        int prev = minv;
+        int res = 0;
+        for(int i=0;i<n-1;i++){
+            if(maxi[i]==-1) continue;
+            
+            res = max(res , mini[i]-prev);
+            prev = maxi[i];
         }
-        return ans;
-        
-        // sort(nums.begin(),nums.end());
-        // int ans = 0;
-        // for(int i=1;i<nums.size();i++){
-        //     ans = max(ans , nums[i]-nums[i-1]);
-        // }
-        // return ans;
+        res = max(maxv - prev , res);
+        return res;
     }
 };
