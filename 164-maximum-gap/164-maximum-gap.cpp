b92b206f3506d2211aa980bb5dd40908c1016c04
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-03-15 09:17:23 +0530

    Time: 330 ms (30.95%), Space: 79.8 MB (30.46%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.cpp
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int ans = 0;
-        for(int i=1;i<nums.size();i++){
-            ans = max(ans , nums[i]-nums[i-1]);
+        
+        priority_queue<int,vector<int>>pq;
+        for(auto i : nums){
+            pq.push(i);
+        }
+        
+        int prev = pq.top() ;
+        pq.pop();
+        int ans= 0;
+        while(!pq.empty()){
+            int a = pq.top();
+            pq.pop();
+            ans = max(ans, abs(a-prev));
+            prev  = a;
         }
         return ans;
+        
+        // sort(nums.begin(),nums.end());
+        // int ans = 0;
+        // for(int i=1;i<nums.size();i++){
+        //     ans = max(ans , nums[i]-nums[i-1]);
+        // }
+        // return ans;
     }
 };
