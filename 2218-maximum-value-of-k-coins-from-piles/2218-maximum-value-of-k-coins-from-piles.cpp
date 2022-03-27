b9f3f3013097969d1cba95dd9e39cb54db5c04d9
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-03-27 10:51:38 +0530

    Time: 208 ms (100.00%), Space: 19.6 MB (16.67%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int dp[1001][2001];
     
-    int check(vector<vector<int>>&piles , int idx , int sidx , int k , int x){
+    int check(vector<vector<int>>&piles , int idx , int k , int x){
         if(idx >= piles.size()) return 0;        
-        // if(piles[idx].size()<=sidx) return 0;
         if(x>=k) return 0;
         
         if(dp[idx][x]!=-1 ) return dp[idx][x];
@@ -16,10 +15,10 @@ public:
         for(int i=0;i<piles[idx].size();i++){
             if(x+i+1>k) break;
             s+=piles[idx][i];
-            ans = max(ans ,  check(piles, idx+1 , sidx , k , x+i+1) + s );
+            ans = max(ans ,  check(piles, idx+1 , k , x+i+1) + s );
         }
         
-        ans = max(ans , check(piles , idx+1 , 0 , k , x));
+        ans = max(ans , check(piles , idx+1 , k , x));
         
         
         return dp[idx][x] =   ans;
@@ -28,6 +27,6 @@ public:
     
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         memset(dp , -1 , sizeof(dp));
-        return check(piles , 0 , 0 , k , 0);
+        return check(piles , 0  , k , 0);
     }
 };
