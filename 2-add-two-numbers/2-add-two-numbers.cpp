572bcefd29ec660a46906c542948d9c92f3b654c
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-07-04 23:26:44 +0530

    Time: 57 ms (45.01%), Space: 71.5 MB (11.54%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,26 +10,87 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int carry = 0;
-        ListNode *head = new ListNode(-1);
-        ListNode *temp = head;
-        while(l1 || l2){
-            int val = ((l1) ? l1->val : 0)  + ((l2) ? l2->val : 0) + carry ; 
-            if(val > 9){
-                carry = val/10;
-                val = val%10;
-            }else{
-                carry = 0 ;
-            }
-            
-            temp->next = new ListNode(val);
-            temp = temp->next;
-            if(l1) l1 = l1->next;
-            if(l2) l2 = l2->next;
+    
+    ListNode *reverse(ListNode *head){
+        ListNode *prev = NULL;
+        while(head){
+            ListNode *t = head->next;
+            head->next = prev;
+            prev = head;
+            head = t;
         }
+        return prev;
+    }
+    
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = NULL;
+        ListNode *temp = NULL;
+        int c = 0;
+        while(l1 && l2){
+            int num = l1->val+ l2->val + c;
+            if(num > 9){
+
+                c = num/10;
+                num = num%10;
+            }else{
+                c= 0;
+            }
+            ListNode *newnode = new ListNode(num);
+            if(!head){
+                head = temp = newnode;
+            }else{
+                temp->next = newnode;
+                temp = newnode;
+            }
+            l1  = l1->next;
+            l2 = l2->next;
+        }
+        while(l1){
+            int num = l1->val + c;
+            if(num > 9){
+                c = num/10;
+                num = num%10;
+                
+            }else{
+                c= 0;
+            }
+            ListNode *newnode = new ListNode(num);
+            if(!head){
+                head = temp = newnode;
+            }else{
+                temp->next = newnode;
+                temp = newnode;
+            }
+            l1 = l1->next;
+        }
+        while(l2){
+            int num =  l2->val + c;
+            if(num > 9){
+                c = num/10;
+                num = num%10;
+                // c = num/10;
+            }else{
+                c= 0;
+            }
+            ListNode *newnode = new ListNode(num);
+            if(!head){
+                head = temp = newnode;
+            }else{
+                temp->next = newnode;
+                temp = newnode;
+            }
+            l2 = l2->next;
+        }
+        if(c!=0){
+            ListNode *newnode = new ListNode(c);
+            if(!head){
+                head = temp = newnode;
+            }else{
+                temp->next = newnode;
+                temp = newnode;
+            }
+        }
+        return (head);
         
-        if(carry > 0) temp->next = new ListNode(carry);
-        return head->next;
     }
 };
