19fddd3c8f47118be7373b0b274442d63b83c16e
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-07-06 12:06:54 +0530

    Time: 106 ms (38.59%), Space: 43 MB (21.02%) - LeetHub

--- 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
-    int kConcatenationMaxSum(vector<int>& arr, int k) {
-        vector<int>ans;
-        
-        long a =0, s=0;
-        int m = 1000000007;
-        
-        int n = arr.size();
-        for(int i=0;i<(k==1 ? n : 2*n);i++){
-            int idx = i%n;
-            s = (s + arr[idx]);
-            a=max(s , a);
+    int mod = 1e9+7;
+    int maxSubarraySum(vector<int>&arr){
+        int s=0;
+        int res = 0;
+        for(auto i : arr){
+            s= (s%mod + i%mod)%mod;
+            res = max(res , s);
             if(s<0){
-                s=0;
+                s = 0;
             }
         }
-        
-        if(k==1 || k==2) return (int)a%m;
-        
-        int  sum=0;
-        for(auto i :arr) sum = (sum + i);
-        if(sum >0){
-            long res = a + ((long)(k-2)*sum);
-            cout<<res;
-            return (int)(res%m);
-            
-        }else{
-            return a;
+        return res;
+    }
+    int kConcatenationMaxSum(vector<int>& arr, int k) {
+        long long  s =0 ;
+        for(auto i : arr) s+=i;
+        if(s<=0){
+            vector<int>arr1;
+            for(auto i : arr){
+                arr1.push_back(i);
+            }
+            for(auto i : arr){
+                arr1.push_back(i);
+            }
+            return max(maxSubarraySum(arr), k>1 ?  maxSubarraySum(arr1) : 0);
         }
-        
+        else{
+            s=((s%mod)*(k-1)%mod)%mod;
+            int n = arr.size();
+            vector<int>a(n,0);
+            a[0] = arr[0];
+            int res = a[0];
+            for(int i=1;i<n;i++){
+                a[i] = arr[i] + a[i-1];
+                res = max(res , a[i]);
+            }
+            int minv = 0;
+            for(int i=n-1;i>=0;i--){
+                if(a[i]<0){
+                    minv = min(a[i],minv);
+                }
+            }
+            return ((s%mod)+(res%mod)+(abs(minv)%mod))%mod;
+        }
     }
 };
