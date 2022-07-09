359de015e4a8bce98169a14c25f9152fc63c9f4c
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-07-09 12:44:43 +0530

    Time: 9 ms (15.21%), Space: 9.9 MB (22.60%) - LeetHub

--- 397-integer-replacement/397-integer-replacement.cpp
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    
-    unordered_map<int,int>m;
-    
-    int check(int n){
-        if(n==1) return 0;
-        
-        if(m.find(n)!=m.end()) return m[n];
-        int ans = n-1;
-        if(n%2==0){
-            ans = min(ans ,  check(n/2)  +1 );
-        }else{
-            ans = min(ans , min( check(n-1) , check(n+1) ) + 1);
-        }
-        return m[n] = ans;
+       map<int,int>dp;
+   long calc(long n)
+    {        
+        if(n==1)
+            return dp[n];
+       if(dp[n]==NULL)
+       {
+        if(n%2==0)
+        dp[n]=calc(n/2)+1;
+        else            
+        dp[n]=min(calc(n-1),calc(n+1))+1;  
+       }       
+        return dp[n];
     }
-    
     int integerReplacement(int n) {
-        // return 0;
-        m[INT_MAX]  = 32;
-        return check(n);
+       return calc((long)n);
     }
 };
