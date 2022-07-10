37f8a6b0bdb2edafb4059a1ceca40bad5b2f6553
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-07-10 10:43:35 +0530

    Time: 8 ms (12.71%), Space: 7.1 MB (78.67%) - LeetHub

--- 355-design-twitter/355-design-twitter.cpp
+++ 355-design-twitter/355-design-twitter.cpp
@@ -1,45 +1,48 @@
 class Twitter {
 public:
-    
-    unordered_map<int,vector<pair<int,int>>>t;
-    unordered_map<int,set<int>>f;
+    unordered_map<int,set<int>>follow1;
+    unordered_map<int,vector<pair<int,int>>>post;
     int time = 0;
     Twitter() {
         
     }
     
     void postTweet(int userId, int tweetId) {
-        t[userId].push_back({ time++ , tweetId });
-        
+        if(post.find(userId)!=post.end()){
+            post[userId].push_back({time++ , tweetId});
+        }else{
+            vector<pair<int,int>>t;
+            t.push_back({time++, tweetId});
+            post[userId] = t;
+        }
     }
     
     vector<int> getNewsFeed(int userId) {
         priority_queue<pair<int,int>>pq;
-        for(auto i : t[userId]){
+        for(auto i : post[userId]){
             pq.push(i);
         }
-        for(auto i : f[userId]){
-            for(auto j : t[i]){
+        for(auto i : follow1[userId]){
+            for(auto j : post[i]){
                 pq.push(j);
             }
         }
-        vector<int>res;
         int k = 10;
-        while(!pq.empty() && k--){
+        vector<int>res;
+        while(!pq.empty() && k ){
             pair<int,int>p = pq.top();
             pq.pop();
             res.push_back(p.second);
+            k-=1;
         }
         return res;
     }
     
     void follow(int followerId, int followeeId) {
-        if(followerId != followeeId){
-            f[followerId].insert(followeeId);
-        }
+        follow1[followerId].insert(followeeId);
     }
     
     void unfollow(int followerId, int followeeId) {
-        f[followerId].erase(followeeId);
+        follow1[followerId].erase(followeeId);
     }
 };
