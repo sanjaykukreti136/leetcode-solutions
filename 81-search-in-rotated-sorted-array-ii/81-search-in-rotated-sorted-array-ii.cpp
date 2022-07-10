7f8426ae3188eba86edfd932748c5be8510f0240
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-07-10 11:37:37 +0530

    Time: 13 ms (32.40%), Space: 14 MB (37.60%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int t) {
-        int i=0, j= nums.size()-1;
-        while(i<=j){
-
-            int mid  = (i+j)/2;
-            if(nums[mid]==t) return true;
-            if(nums[i] < nums[mid]){
-                if(t>=nums[i] && t<=nums[mid]) j = mid-1;
-                else{
-                    i = mid+1;
-                }
+    bool search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        while(l <= r)
+        {
+            int mid = l + (r-l) / 2;
+            if (nums[mid] == target)
+                return true;
+			// with duplicates we can have this contdition, just update left & right
+            if((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
+            {
+                l++;
+                r--;
             }
-            else if(nums[i]==nums[mid]){
-                i++;
+			// first half
+			// first half is in order
+            else if(nums[l] <= nums[mid])
+            {
+				// target is in first  half
+                if((nums[l] <= target) && (nums[mid] > target))
+                    r = mid - 1;
+                else
+                    l = mid + 1;
             }
-            else if(nums[mid]<=nums[j]){
-                if(t>=nums[mid] && t<=nums[j]) i = mid+1;
-                else{
-                    j = mid-1;
-                }
+			// second half
+			// second half is order
+			// target is in second half
+            else
+            {
+                if((nums[mid] < target) && (nums[r]>= target))
+                    l = mid + 1;
+                else
+                    r = mid - 1;
             }
         }
         return false;
     }
+	// for github repository link go to my profile.
 };
