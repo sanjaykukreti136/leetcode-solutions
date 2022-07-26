61d1434331c3d3e5fdc1779f7073d942c88163b3
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-07-26 14:37:41 +0530

    Time: 127 ms (28.46%), Space: 21.6 MB (5.02%) - LeetHub

---
+++ 221-maximal-square/221-maximal-square.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n = matrix.size(), m=matrix[0].size();
+        vector<vector<int>>dp(n ,vector<int>(m, 0));
+        vector<vector<int>>dp1(n ,vector<int>(m, 0));
+        vector<vector<int>>dp2(n ,vector<int>(m, 0));
+        for(int i= n-1;i>=0;i--){
+            int s = 0;
+            for(int j=m-1;j>=0;j--){
+                // s+=matrix[i][j]-'0';
+                if(matrix[i][j]=='1') s+=1;
+                else s = 0;
+                dp1[i][j] = s;
+            }
+        }
+        
+        for(int j=m-1;j>=0;j--){
+            int s = 0;
+            for(int i=n-1;i>=0;i--){
+                // s+=matrix[i][j]-'0';
+                 if(matrix[i][j]=='1') s+=1;
+                else s = 0;
+                dp2[i][j] = s;
+            }
+        }
+        int res = 0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 || j==m-1){
+                    dp[i][j] = matrix[i][j]-'0';
+                }else{
+                    if(matrix[i][j]=='1'){
+                        
+                        dp[i][j] = min( dp1[i][j] , min(dp2[i][j] , dp[i+1][j+1]+1) );
+                        // if(dp1[i+1][j]==dp[i+1][j+1] && dp2[i][j+1]==dp[i+1][j+1]){
+                        //     dp[i][j] = dp1[i+1][j];
+                        // }
+                        // dp[i][j] = max(dp[i][j], 1);
+                    }else{
+                        dp[i][j] = 0;
+                    }
+                }
+                res = max(res , dp[i][j]);
+            }
+        }
+        
+        return res*res;
+        
+        
+    }
+};
