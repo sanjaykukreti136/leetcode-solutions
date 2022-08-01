7120b8bad9f3e2649b2437955f09b589752fe01d
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-08-01 13:18:36 +0530

    Time: 3 ms (72.22%), Space: 8.8 MB (10.16%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,46 +1,67 @@
 class Solution {
 public:
     
-    void check(string src , string des , set<string>&v , map<string,vector<pair<string,double>>>&mp , double &ans , double temp ){
+    
+    double check(string src , string des , unordered_map<string,vector<pair<string,double>>>mp){
         
-        if(v.find(src)!=v.end()) return ;
+        if(mp.find(src)==mp.end()) return -1;
+        if(mp.find(des)==mp.end()) return -1;
         
-        if(src == des){
-            ans = temp;
-            return ;
-        }
-        v.insert(src);
-        for(auto i : mp[src]){
+        queue<pair<string,double>>q;
+        set<string>st;
+        st.insert(src);
+        q.push({src , 1});
+        while(!q.empty()){
             
-            check(i.first , des , v , mp , ans , temp*i.second);
+            pair<string,double>p = q.front();
+            q.pop();
+            string point = p.first;
+            double curr= p.second;
+            if(point == des){
+                return curr;
+            }
+            
+            for(auto i : mp[point]){
+                
+                if(st.find(i.first)==st.end()){
+                    st.insert(i.first);
+                    q.push({i.first , curr*i.second});
+                }
+                
+            }
+            
+            
             
         }
+        return -1;
         
         
     }
     
-    
-    vector<double> calcEquation(vector<vector<string>>& e, vector<double>& v, vector<vector<string>>& q) {
-        map<string,vector<pair<string,double>>>mp;
-        int k =0;
-        for(auto i : e){
-            mp[i[0]].push_back({ i[1] , v[k] });
-            mp[i[1]].push_back({ i[0] , 1/v[k++] });
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        
+        unordered_map<string,vector<pair<string,double>>>mp;
+        int k = 0;
+        for(auto i : equations){
+            string start = i[0];
+            string end = i[1];
+            double value = values[k++];
+            mp[start].push_back({end , value});
+            mp[end].push_back({start , 1/value});
         }
         
-        vector<double>res;
+        vector<double>ans;
         
-        for(auto i : q){
+        for(auto i : queries ){
             string src = i[0];
             string des = i[1];
-            double ans = -1.0 , temp = 1.0;
-            set<string>v;
-            if(mp.find(src)!=mp.end())
-            check(src , des , v , mp , ans , temp );
-            res.push_back(ans);
+            
+            ans.push_back(check(src  , des , mp));
             
         }
-        return res;
+        return ans;
+        
+        
         
     }
 };
