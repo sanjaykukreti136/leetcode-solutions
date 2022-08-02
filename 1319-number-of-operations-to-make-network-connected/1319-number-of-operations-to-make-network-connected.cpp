7e28e5dfa2ef8c7b68baa4cf3a2a3ce28329cc43
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-08-02 12:11:53 +0530

    Time: 214 ms (54.43%), Space: 42.3 MB (81.65%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
-    int findParent(int node,vector<int>&parent)
-    {
-        if(parent[node]==node) return node;
-        return parent[node]=findParent(parent[node],parent);
+    int parent[100005];
+    int rank[100005];
+    
+    int find_par(int a){
+        if(a == parent[a]) return a;
+        return parent[a] = find_par(parent[a]);
     }
     
-    void _union( int u,int v,vector<vector<int>>&graph,vector<int>&parent, vector<int>&rank)
-    {
-        u=findParent(u, parent);
-        v=findParent(v, parent);
-        if(rank[u]<rank[v])
-        {
-            parent[u]=v;
-        }else if(rank[u]>rank[v])
-        {
-            parent[v]=u;
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+            rank[i] = 1;
         }
-        else if(rank[u]==rank[v])
-        {
-            parent[v]=u;
-            rank[u]++;
-        }
-    }
-    int makeConnected(int n, vector<vector<int>>& graph) {
-        vector<int> parent(n, 0), rank(n,0);
-        for(int i=0;i<n;i++)
-            parent[i]=i;
-        int cnt=0;
-        for(int i=0;i<graph.size();i++)
-        {
-            int a=graph[i][0];
-            int b=graph[i][1];
-            
-            if(findParent(a, parent)==findParent(b,parent)) continue;
-            else
-            {
-                _union(a,b,graph,parent,rank);
-                cnt++;
+        
+        
+        for(auto i : connections){
+            int a = i[0];
+            int b = i[1];
+            a = find_par(a);
+            b = find_par(b);
+            if(a!=b){
+                
+                if(rank[a]  < rank[b] ){
+                    rank[a]+=rank[b];
+                    parent[b] = a;
+                }else{
+                    rank[b]+=rank[a];
+                    parent[a] = b;
+                }
+                
             }
         }
-       return graph.size() < n-1 ? -1 : n-1-cnt;
-
+        int c = 0;
+        for(int i=0;i<n;i++){
+            if(parent[i]==i) c++;
+        }
+        if(connections.size() < n-1) return -1;
+        return c==1 ?  0  : c-1;
+        // if()
+        
+        
     }
 };
