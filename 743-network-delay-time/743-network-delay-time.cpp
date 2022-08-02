22a11ebc75d03c490d6958de0193a51134b76770
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-08-02 19:34:19 +0530

    Time: 246 ms (30.08%), Space: 44.7 MB (18.55%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,64 +1,45 @@
-class pair1{
-    public:
-     int first;
-     int second;
-     pair1(){}
-     pair1(int u, int v){
-         first=  u;
-         second = v;
-     }
-};
-
-class compare{
-    public :
-     bool operator()(pair1 *a , pair1 *b){
-         return a->second > b->second;
-     }
-};
-
 class Solution {
 public:
-    
-    
-    
-    int ans = INT_MIN;
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        
         vector<vector<pair<int,int>>>graph(n);
-        
-        for(int i=0;i<times.size();i++){
-            int s = times[i][0]-1;
-            int d = times[i][1]-1;
-            int v = times[i][2];
-            graph[s].push_back({d,v});
+        for(auto i : times){
+            graph[i[0]-1].push_back({i[2] , i[1]-1});
         }
-        vector<bool>vis(n , false);
-        priority_queue<pair1*, vector<pair1*>, compare>pq;
         
-        pq.push(new pair1(k-1 , 0));
-        // vis[k-1]=true;
-        int s=0;
+        k-=1;
+        // for(auto i : graph[k]){
+        //     pq.push(i);
+        // }
+        pq.push({ 0  , k });
+        vector<int>vis(n, -1);
+        // vis[k] = 0;
+        int res = 0;
         while(!pq.empty()){
-            pair1 *p = pq.top();
+            pair<int,int>p = pq.top();
             pq.pop();
             
-            int src = p->first;
-            int wt = p->second;
-            if(vis[src]==true) continue;
-            vis[src] = true;
-            // s+=wt;    
-            ans = max(ans , wt);
-            for(auto i : graph[src]){
-                if(vis[i.first]==false){
-                    pq.push(new pair1(i.first ,wt + i.second  ));
+            int node = p.second;
+            if(vis[node]!=-1) continue;
+            cout<<node<<"\n";
+            int val = p.first;
+            vis[node] = val;
+            
+            for(auto i : graph[node]){
+                if(vis[i.second]==-1){
+                    pair<int,int>x = { i.first + val , i.second};
+                    pq.push(x);
                 }
             }
             
         }
         
-        for(int i=0;i<n;i++){
-            if(vis[i]==false) return -1;
+        for(auto i : vis){
+            if(i==-1) return -1;
+            res = max(res, i);
         }
-        return ans;
+        return res;
         
     }
 };
