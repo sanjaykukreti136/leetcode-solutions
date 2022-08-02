cbd555ff1d825525ea4e0e81764d6275e04d151c
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-08-02 14:02:13 +0530

    Time: 908 ms (69.19%), Space: 203.6 MB (10.89%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,53 +1,44 @@
 class Solution {
 public:
-    vector<int> parent, disc, low;
-    vector<bool> visited;
-    vector<vector<int>> ans;
-
-    void dfs(int idx, int parent, vector<vector<int>> &edges, int &time)
-    {
-        disc[idx] = low[idx] = time;
-        time++;
-        visited[idx] = true;
-
-        for (auto it : edges[idx])
-        {
-            if (it == parent)
-                continue;
-            if (visited[it] == true)
-            {
-                low[idx] = min(low[idx], disc[it]);
+    vector<vector<int>>ans;
+    vector<int>low,time,vis;
+    int t = 1;
+    
+    void dfs(vector<vector<int>>&graph , int src , int parent = -1){
+        
+        vis[src] = true;
+        low[src] = time[src] = t++;
+        for(auto i : graph[src]){
+            if(i==parent) continue;
+            
+            if(!vis[i])
+                dfs(graph , i , src);
+            
+            low[src] = min(low[src] , low[i]);
+            if(time[src] < low[i]){
+                vector<int>t = { src  , i };
+                ans.push_back(t);
             }
-            else
-            {
-                dfs(it, idx, edges, time);
-                low[idx] = min(low[idx], low[it]);
-                if (low[it] > disc[idx])
-                {
-                    ans.push_back({idx, it});
-                }
-            }
+            
         }
+        
+        
     }
-
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& conn) {
-        vector<vector<int>> edges(n);
-        for(int i = 0; i<conn.size(); i++){
-           edges[conn[i][0]].push_back(conn[i][1]);
-           edges[conn[i][1]].push_back(conn[i][0]);
-        }
-
-        disc.resize(n);
+    
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         low.resize(n);
-        visited.resize(n, false);
-        int time = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (!visited[i])
-            {
-                dfs(i, -1, edges, time);
-            }
+        time.resize(n);
+        vis.resize(n,0);
+        vector<vector<int>>graph(n);
+        for(auto i : connections){
+            graph[i[0]].push_back(i[1]);
+            graph[i[1]].push_back(i[0]);
         }
+        
+        dfs(graph , 0);
+        
         return ans;
+        
+        
     }
 };
