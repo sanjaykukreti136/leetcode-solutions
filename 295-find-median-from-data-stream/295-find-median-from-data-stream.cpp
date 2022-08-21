ac688270aad858428dcad5449503e8f10be4ca63
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-08-21 18:50:18 +0530

    Time: 601 ms (27.79%), Space: 116.9 MB (89.57%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,74 +1,60 @@
 class MedianFinder {
 public:
-     priority_queue<int> left; //1st half (left half)
-    priority_queue<int,vector<int>,greater<int>> right;
+    
+    priority_queue<int,vector<int>,greater<int>>right;
+    priority_queue<int>left;
     
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        
         if(left.size()==0){
             left.push(num);
-        }
-        else if(left.size()== right.size()){
+        }else if(left.size()==right.size()){
             
-            if(num<=right.top()){
-                left.push(num);
-            }
-            else{
-                
-                int t= right.top();
+            if(num > right.top()){
+                int t = right.top();
                 right.pop();
                 left.push(t);
                 right.push(num);
-                
+            }else{
+                left.push(num);
             }
             
-        }
-        else{
+        }else{
+            
             if(right.size()==0){
                 if(num >= left.top()){
                     right.push(num);
+                }else{
+                    int t = left.top();
+                    left.pop();
+                    left.push(num);
+                    right.push(t);
                 }
-                else{
-                int t= left.top();
-                left.pop();
-                left.push(num);
-                right.push(t);
+            }else{
                 
-                }
-            }
-            else if( num >= right.top() ){
-                right.push(num);
-            }
-            else{
-                if( num <left.top() ){
-                int t= left.top();
-                left.pop();
-                left.push(num);
-                right.push(t);
-                }
-                else{
+                if(num >= left.top()){
                     right.push(num);
+                }else{
+                    int t = left.top();
+                    left.pop();
+                    left.push(num);
+                    right.push(t);
                 }
+                
             }
-            
-            
             
         }
-        
     }
     
     double findMedian() {
-        int lsize = left.size();
-        int rsize = right.size();
-        cout<<lsize<<" - "<<rsize<<"\n";
-        if(lsize > rsize)  //Return top of maxheap for odd no of elements
-            return double(left.top());
-        else    //Else return avg of top of maxheap and minheap
-            return (double(left.top())+double(right.top()))/2;
+        if(left.size()==right.size()){
+            return (double)(left.top() + right.top())/2.0;
+        }else{
+            return (double)(left.top());
+        }
     }
 };
 
