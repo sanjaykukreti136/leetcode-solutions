cd44b61ab22dd3adc14027a745d6c1a04084a62c
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-08-23 09:25:02 +0530

    Time: 46 ms (71.31%), Space: 12.6 MB (39.18%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,55 +1,63 @@
 class Solution {
 public:
     
-    void check(vector<vector<bool>>&vis , vector<vector<char>>&grid , int sr , int sc){
+    void check(int r , int c , vector<vector<char>>&grid , vector<vector<bool>>&vis){
         
+        vis[r][c] = true;
         
-        queue<pair<int,int>>q;
+        grid[r][c] = '.';
         
-        vis[sr][sc] = true;
-        q.push({sr , sc  });
-        while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            if(r-1>=0 && vis[r-1][c]==false && grid[r-1][c] == '1' ){
-                q.push({r-1 , c});
-                vis[r-1][c] = true;
-            }
-            if(c-1>=0 && vis[r][c-1] == false && grid[r][c-1]=='1'){
-                q.push({r , c-1});
-                vis[r][c-1] = true;
-            }
-            if(c+1 < grid[0].size() && vis[r][c+1]==false && grid[r][c+1]=='1'){
-                q.push({r,  c+1});
-                vis[r][c+1] = true;
-            }
-            if(r+1 < grid.size()  && vis[r+1][c]==false && grid[r+1][c]=='1'){
-                q.push({r+1 , c});
-                vis[r+1][c] = true;
-            }
-            
+        // top
+        if(r-1>=0 && vis[r-1][c]==false && grid[r-1][c]=='1'){
+            check(r-1 , c , grid , vis);
+        }
+        // bottom
+        if(r+1<grid.size() && vis[r+1][c]==false && grid[r+1][c]=='1'){
+            check(r+1 , c , grid , vis);
         }
         
+        // left
+        if(c-1>=0 && vis[r][c-1]==false && grid[r][c-1]=='1'){
+            check(r , c-1 , grid , vis);
+        }
+        // right
+        if(c+1<grid[0].size() && vis[r][c+1]==false && grid[r][c+1]=='1'){
+            check(r , c+1 , grid , vis);
+        }
         
         
     }
     
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<bool>>vis(n, vector<bool>(m , false));
-        int c = 0 ;
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]==false && grid[i][j]=='1'){
-                    check(vis , grid , i , j);
+        int n =  grid.size() , m = grid[0].size();
+        vector<vector<bool>>visited(n , vector<bool>(m , false));
+//         for(int i=0;i<grid.size();i++){
+//             for(int j=0;j<grid[0].size();j++){
+                
+//                 if(i==0 || i==grid.size()-1 || j==0 || j==grid[0].size()-1 || grid[i][j]=='1'){
+                    
+//                     check(i , j , grid , visited );
+                    
+//                 }
+                
+//             }
+//         }
+        int c = 0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1'){
+                    // cout<<i<<" "<<j<<"\n";
                     c++;
+                    check(i , j , grid , visited);
                 }
             }
         }
         
-        return c;
+        return c; 
         
     }
 };
+
+
+
