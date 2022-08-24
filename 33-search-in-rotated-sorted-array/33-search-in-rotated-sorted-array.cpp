e410dc0cf7f51a2186ea2ca3564d0ed82e25761a
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-08-24 13:25:58 +0530

    Time: 4 ms (73.01%), Space: 11.2 MB (29.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,24 +1,52 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int t) {
-        int i=0, j= nums.size()-1;
+    
+    
+    int check(vector<int>nums){
+        int i= 0, j = nums.size()-1;
+        int ans = INT_MAX;
+        int idx = -1;
         while(i<=j){
-
-            int mid  = (i+j)/2;
-            if(nums[mid]==t) return mid;
-            
-            if(nums[i] <= nums[mid]){
-                if(t>=nums[i] && t<=nums[mid]) j = mid-1;
-                else{
-                    i = mid+1;
-                }
-            }else if(nums[mid]<=nums[j]){
-                if(t>=nums[mid] && t<=nums[j]) i = mid+1;
-                else{
-                    j = mid-1;
-                }
+            int mid =  (i+j)/2;
+            if(nums[mid] < ans || (nums[mid]==ans && mid<idx) ){
+                ans = nums[mid];
+                idx = mid;
+            }
+            if(nums[mid] > nums[j]){
+                i = mid+1;
+            }else{
+                j = mid-1;
+            }
+        }
+        return idx;
+    }
+    
+    int search(vector<int>& nums, int t) {
+        int mid = check(nums);
+        
+        int s = 0 , e = nums.size()-1;
+        int start = -1 , end = -1;
+        
+        if(t>=nums[mid] && t<=nums[e]){
+            start = mid;
+            end = e;
+        }else{
+            if(mid-1 < 0) return -1;
+            start = 0;
+            end = mid-1;
+        }
+        cout<<start<<" "<<end<<"\n";
+        while(start<=end){
+            int m = (start+end)/2;
+            if(nums[m]==t) return m;
+            else if(nums[m] < t){
+                start = m+1;
+            }else{
+                end = m-1;
             }
         }
         return -1;
+        
+        
     }
 };
