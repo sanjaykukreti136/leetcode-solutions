d67bc597ae5c23c488fd321af96ef2da4c9a19eb
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-08-31 09:41:39 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (48.87%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,9 +3,9 @@ public:
     int rob(vector<int>& nums) {
         int inc = 0, exc = 0;
         for(auto i : nums){
-            int t = i+exc;
-            exc = max(inc , exc);
-            inc = t;
+            int t = inc;
+            inc = exc + i;
+            exc = max(exc , t);
         }
         return max(inc , exc);
     }
