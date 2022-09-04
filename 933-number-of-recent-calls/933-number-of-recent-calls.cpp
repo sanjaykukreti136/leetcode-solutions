a661fa6be864e0d6dc34dddabb6d3f2576f33edb
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-09-04 06:51:54 +0530

    Time: 380 ms (10.21%), Space: 58.1 MB (18.12%) - LeetHub

---
+++ 933-number-of-recent-calls/933-number-of-recent-calls.cpp
@@ -0,0 +1,37 @@
+class RecentCounter {
+private:
+    vector<int> res;
+    
+    int getIndexOfOrGre(int n){//index of n or greater than n , or -1 if both fails
+        int low=0,high=res.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(res[mid]==n)
+                return mid;
+            else if(res[mid]>n){
+                if(mid-1>=0 && res[mid-1]<n)
+                    return mid;
+                high=mid-1;
+            }else{
+                if(mid+1<res.size() && res[mid+1]>n)
+                    return mid+1;
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+public:
+    RecentCounter() {
+    }
+    
+    int ping(int t) {
+        res.push_back(t);
+        int ans=0;
+        int tstart=t-3000;
+        int i=getIndexOfOrGre(tstart);
+        if(i<0)
+            return res.size();
+        else
+            return res.size()-i;
+    }
+};
