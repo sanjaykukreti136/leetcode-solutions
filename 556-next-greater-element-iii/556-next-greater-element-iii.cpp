c7c22d5b769eccfccd53e5a30dcc248c3444a0dc
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-09-04 11:52:57 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.48%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -2,36 +2,29 @@ class Solution {
 public:
     int nextGreaterElement(int n) {
         string s = to_string(n);
-        int j = -1;
-        for(int i=s.length()-1;i>0;i--){
-            if(s[i]>s[i-1]){
-                j = i-1;
+        int size=  s.length();
+        int idx = -1;
+        for(int i=size-2;i>=0;i--){
+            if(s[i]<s[i+1]){
+                idx = i;
                 break;
             }
         }
-        if(j==-1) return -1;
-        char ch = 'A';
-        int idx = -1;
-        cout<<j<<"\n";
-        for(int i=s.length()-1;i>j;i--){
-            if(s[i]>s[j] && s[i]<ch){
-                ch = s[i];
-                idx = i;
+        if(idx == -1) return -1;
+        int nidx = -1, val  = INT_MAX ;
+        for(int i=idx+1;i<size;i++){
+            if((s[i] > s[idx] )&& (s[i]-'0')<=val  ){
+                nidx = i;
+                val = s[i]-'0';
             }
         }
-        if(idx == -1) return -1;
-        cout<<idx;
-        swap(s[j]  , s[idx]);
-        j= j+1;
-        idx = s.length()-1;
-        while(j<=idx){
-            swap(s[j], s[idx]);
-            j++;
-            idx--;
-        }
-        // return (int)stol(s);
-        long a = stol(s);
-        if(a>INT_MAX || a<n) return -1;
-        return (int)a;
+        if(nidx == -1) return -1;
+        swap(s[idx] , s[nidx]);
+        reverse(s.begin() + idx+1, s.end());
+        long long x = stol(s);
+        int res = pow(2 , 31)-1;
+        if(x > res ) return -1;
+        return stoi(s);
+        
     }
 };
