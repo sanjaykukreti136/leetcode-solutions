6f4b065fe517f4112a8e4e1a8093b9fbb94b2967
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-09-04 11:50:47 +0530

    Time: 19 ms (66.01%), Space: 15.9 MB (54.98%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,54 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int c1 = 0 , val1 = INT_MIN;
-        int c2 = 0 , val2 = INT_MIN;
-        
-        
-         // 1 
-        // x-y   
-         
-//           1 2 
-//           1 -1 
-//           2- 1
+        int c1 = 0, ans1 = INT_MIN;
+        int c2 = 0, ans2 = INT_MIN;
         
         for(auto i : nums){
-            if(i==val1){
+            
+            if(i==ans1){
                 c1++;
+                ans1 = i;
             }
-            else if(i==val2){
+            else if(i==ans2){
                 c2++;
-            }else{
-
+                ans2 = i;
+            }
+            else{
                 if(c1==0){
-                    c1 = 1;
-                    val1 = i;
-                }
-                else if(c2==0){
+                    ans1 = i;
+                    c1=1;
+                }else if(c2 == 0){
+                    ans2 = i;
                     c2 = 1;
-                    val2 = i;
                 }else{
-                    c1--;
-                    c2--;
+                c1--;
+                c2--;
                 }
             }
+            
         }
-        int c = 0;
-        vector<int>ans;
-        for(auto i : nums){
-            if(i==val1) c++;
-        }
-        if(c>nums.size()/3){
-            ans.push_back(val1);
-        }
-        c=0;
-        // if(val1 == val2) return ans;
-        for(auto i : nums){
-            if(i==val2) c++;
-        }
-        if(c>nums.size()/3){
-            ans.push_back(val2);
-        }
-        return ans;
-     }
+        cout<<ans1<<" "<<ans2<<"\n";
+        c1 = 0;
+        for(auto i : nums) if(i==ans1) c1++;
+        c2 = 0;
+        for(auto i : nums) if(i==ans2) c2++;
+        vector<int>res;
+        if(c1 > nums.size()/3) res.push_back(ans1);
+        if(c2 > nums.size()/3) res.push_back(ans2);
+        return res;
+        
+    }
 };
