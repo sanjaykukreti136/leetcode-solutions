e9c443884905a810d22814aee141f1b9756aef28
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-09-06 06:16:58 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (74.90%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,52 +1,27 @@
 class Solution {
 public:
-    
-    
-    int check(vector<int>nums){
-        int i= 0, j = nums.size()-1;
-        int ans = INT_MAX;
-        int idx = -1;
-        while(i<=j){
-            int mid =  (i+j)/2;
-            if(nums[mid] < ans || (nums[mid]==ans && mid<idx) ){
-                ans = nums[mid];
-                idx = mid;
-            }
-            if(nums[mid] > nums[j]){
-                i = mid+1;
-            }else{
-                j = mid-1;
-            }
-        }
-        return idx;
-    }
-    
-    int search(vector<int>& nums, int t) {
-        int mid = check(nums);
+    int search(vector<int>& nums, int target) {
+        int s =0 , e = nums.size()-1;
         
-        int s = 0 , e = nums.size()-1;
-        int start = -1 , end = -1;
-        
-        if(t>=nums[mid] && t<=nums[e]){
-            start = mid;
-            end = e;
-        }else{
-            if(mid-1 < 0) return -1;
-            start = 0;
-            end = mid-1;
-        }
-        cout<<start<<" "<<end<<"\n";
-        while(start<=end){
-            int m = (start+end)/2;
-            if(nums[m]==t) return m;
-            else if(nums[m] < t){
-                start = m+1;
-            }else{
-                end = m-1;
+        while(s <= e){
+            int mid = (s+e)/2;
+            if(target==nums[mid]) return mid;
+            if(nums[s] <= nums[mid]){
+                if(target>=nums[s] && target<=nums[mid]){
+                    e = mid-1;
+                }
+                else{
+                    s = mid+1;
+                }
+            }
+            else{
+                if(target>=nums[mid] && target<=nums[e]){
+                    s = mid+1;
+                }else{
+                    e = mid-1;
+                }
             }
         }
         return -1;
-        
-        
     }
 };
