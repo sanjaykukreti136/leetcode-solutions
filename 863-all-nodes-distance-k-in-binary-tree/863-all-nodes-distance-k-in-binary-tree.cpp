967a29489b1d053e3ae6c2294c3e2ecc137dd372
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-09-09 09:22:08 +0530

    Time: 11 ms (52.84%), Space: 12.4 MB (93.60%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,98 +9,40 @@
  */
 class Solution {
 public:
+    vector<int>res;
     
-    void check2(TreeNode *curr , int k , vector<int>&ans , TreeNode *block){
-        if(!curr || k<0 || curr==block) return ;
+    void check1(TreeNode *root , TreeNode *block , int d){
+        if(!root || root==block) return;
+        if(d==0) res.push_back(root->val);
+        check1(root->left , block , d-1);
+        check1(root->right , block , d-1);
         
-        if(k==0){
-            ans.push_back(curr->val);
-        }
-        check2(curr->left , k-1, ans ,block );
-        check2(curr->right , k-1, ans , block);
     }
     
-    int check1(TreeNode *root , TreeNode *target , int k , vector<int>&ans){
-        if(!root) return -1;
+    int check(TreeNode *root , TreeNode *target , int k){
+        if(!root) return 0;
         
-        if(root == target){
-            check2(root ,  k , ans , NULL);
+        if(root==target){
+            check1(root , NULL , k);
             return 1;
         }
         
-        int l = check1(root->left , target , k , ans);
-        
-        if(l!=-1){
-            check2(root , k-l , ans , root->left);
-            return l+1;
+        int x = check(root->left , target , k);
+        if(x > 0){
+            check1(root , root->left , k-x);
+            return x+1;
         }
-        
-        int r = check1(root->right , target , k , ans);
-        if(r!=-1){
-            check2(root , k-r , ans , root->right);
-            return r+1;
+        int y = check(root->right , target , k);
+        if(y > 0){
+            check1(root, root->right , k-y);
+            return y+1;
         }
-        return -1;
+        return 0; 
         
     }
-
-    vector<int>distanceK(TreeNode *root , TreeNode *target , int k){
-          vector<int>ans;
-          check1(root , target , k  ,ans);
-          return ans;
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        check(root , target , k);
+        return res;
     }
-    
-//     void check(TreeNode *root , unordered_map<TreeNode*, TreeNode*>&m){
-//         queue<TreeNode*>q;
-//         q.push(root);
-//         while(!q.empty()){
-//             TreeNode *t = q.front();
-//             q.pop();
-//             if(t->left){
-//                 m[t->left] = t;
-//                 q.push(t->left);
-//             }
-//             if(t->right){
-//                 m[t->right] = t;
-//                 q.push(t->right);
-//             }
-//         }
-//     }
-    
-//     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-//         unordered_map<TreeNode*,TreeNode*>parent;
-//         check(root , parent);
-        
-//         queue<TreeNode*>q;
-//         unordered_map<TreeNode*,bool>vis;
-//         q.push(target);
-//         vis[target] = true;
-//         int lev= 0;
-//         while(!q.empty()){
-//             int size = q.size();
-//             if(lev++==k) break;
-//             while(size--){
-//                 TreeNode *t = q.front();
-//                 q.pop();
-//                 if(t->left && !vis[t->left]){
-//                     vis[t->left] = true;
-//                     q.push(t->left);
-//                 }
-//                 if(t->right && !vis[t->right]){
-//                     vis[t->right] = true;
-//                     q.push(t->right);
-//                 }
-//                 if(parent[t] && !vis[parent[t]] ){
-//                     vis[parent[t]] = true;
-//                     q.push(parent[t]);
-//                 }
-//             }
-//         }
-//         vector<int>ans;
-//         while(!q.empty()){
-//             ans.push_back(q.front()->val);
-//             q.pop();
-//         }
-//         return ans;
-//     }
 };
