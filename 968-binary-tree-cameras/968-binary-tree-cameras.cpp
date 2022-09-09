7b816c77e297770f0a2207c0f59d2a7100a70068
Sanjay Kukreti <65654176+sanjaykukreti136@users.noreply.github.com>
2022-09-09 10:59:49 +0530

    Time: 26 ms (26.32%), Space: 21.2 MB (85.80%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -11,27 +11,26 @@
  */
 class Solution {
 public:
-    // 0 means covered
-    // -1 means required
-    // 1 means have camera
-        int c = 0;
+    int ans = 0;
     int check(TreeNode *root){
         if(!root) return 0;
         if(!root->left && !root->right) return -1;
         
         int l = check(root->left);
         int r = check(root->right);
-        if(l==-1 || r==-1) {
-            c++;
+        if(l==-1 || r==-1){
+            ans+=1;
             return 1;
         }
-        if(l==1 || r==1) return 0;
+        if(l==1 || r==1){
+            return 0;
+        }
         return -1;
     }
+    
     int minCameraCover(TreeNode* root) {
-        if(!root->left && !root->right) return 1;
-        int x = check(root);
-        if(x==-1) c++;
-        return c;
+        int x =  check(root);
+        if(x==-1) ans+=1;
+        return ans;
     }
 };
